007672304e8cc34bf623329b8a061780ec9e0d6b
Amikom <salsabilasyifa4091@gmail.com>
2024-06-05 10:32:48 +0700

    push

--- app/src/main/java/com/example/perentalan/HomeActivity.kt
+++ app/src/main/java/com/example/perentalan/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ImageView
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
